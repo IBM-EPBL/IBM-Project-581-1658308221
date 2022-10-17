60d20f6fb233055ed160e99bacff1d86a07b1b91
JanakaMurugeshJ <113749186+JanakaMurugeshJ@users.noreply.github.com>
2022-10-17 21:33:09 +0530

    Add files via upload

---
+++ Assignment/Team Leader/Assignment 3/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, url_for, request 
+app = Flask(_name) 
+@app.route('/success/<name>') 
+def success(name): 
+    return 'welcome %s' % name 
+@app.route('/login',methods = ['POST', 'GET']) 
+def login(): 
+    if request.method == 'POST': 
+        user = request.form['nm'] 
+        return redirect(url_for('success',name = user)) 
+        else: 
+            user = request.args.get('nm') 
+            return redirect(url_for('success',name = user)) 
+            if __name_ == '_main_': app.run(debug = True)
+            
